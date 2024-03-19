78280082688e62c9273a13016f0ec2ad1e1a1bb2
necati148 <necati148@RPC11>
2024-03-19 10:19:31 +0300

    Proje dosyası ekle.

---
+++ TakimListesi/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakimListesi
+{
+    public partial class Form1 : Form
+    {
+        string[,] takımlar = new string[4, 3]
+        {
+            {"samet","sadettin","selim"},
+            {"muzaffer","eyüp","özlem" },
+            {"erdem","mahmut","bedirhan"},
+            {"mehmet","burak","ayşe" }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(takımlar[3, 0]+" "+ takımlar[3, 1]+" "+ takımlar[3,2]);
+            
+
+        }
+    }
+}
